00d9656925ce25996f3a3230e25c5e062e5532db
Kennedy <kennedymessias.vb@gmail.com>
2020-01-23 13:22:06 -0300

    adding overall statistics

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,19 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.onCreateMatch = functions.firestore.document('/matches/{matchId}').onCreate( dataComming => {
-   let createdData = dataComming.data();
+   const createdData = dataComming.data();
    let { players } = dataComming.data();
    try {
       players = recalculatePlayersPoints(players, createdData.winnerSide);
       players.forEach(player => {
-      admin.firestore().doc(`users/${player.id}`).update(player);
-      const point = {
-         date: createdData.date,
-         playerId: player.id,
-         points: player.points
-      }
-      const parsedDate = new Date(createdData.date._seconds * 1000).toDateString();
-      admin.firestore().doc(`points/${player.id}-${parsedDate}`).set(point);
+         admin.firestore().doc(`users/${player.id}`).update(player);
+         const point = {
+            date: createdData.date,
+            playerId: player.id,
+            points: player.points
+         }
+         const parsedDate = new Date(createdData.date._seconds * 1000).toDateString();
+         admin.firestore().doc(`points/${player.id}-${parsedDate}`).set(point);
       });
    } catch (error) {
       console.log(error);
@@ -24,36 +24,56 @@ exports.onCreateMatch = functions.firestore.document('/matches/{matchId}').onCre
 });
 
 exports.createUniqueUser = functions.https.onRequest(async (req, res) => {
-try {
+   try {
+      const user = req.body;
+      const result = await admin.firestore().collection('users').where('name', '==', `${user.name}`).get();
+      if(result.docs.length == 0){
+         const userCreated = await createUserAuthentication(user);
+         const response = await createOfficialUser(user, userCreated.uid);
+         return res.status(200).json('Usuário criado com sucesso!');
+      }
+      else {
+         return res.status(400).json('Nome de usuário já existente!');
+      }
+   }
+   catch(error) {
+      console.log(error);
+      return res.status(500);
+   }
+});
+
+exports.getOverallStatistics = functions.https.onRequest(async (req, res) => {
+   const today = new Date();
    const user = req.body;
-   const result = await admin.firestore().collection('users').where('name', '==', `${user.name}`).get();
-   if(result.docs.length == 0){
-      const userCreated = await createUserAuthentication(user);
-      await createOfficialUser(user, userCreated.uid);
-      return res.status(200).json('Usuário criado com sucesso!');
+   console.log(user)
+   try{
+      const userMatches = (admin.firestore().collection('matches').where('players', 'array-contains', `${user.name}`)
+      .get()).docs.map(x => x.data());
+      const pointsPerDay = (await admin.firestore().collection('points').where('playerId', '==', `${user.id}`).get()).docs.map(x => x.data());
+      const wonMatches = userMatches.filter(x => x.winners.includes(user.name));
+      const totalWonMatches = wonMatches.length;
+      const totalWonMatchesThisMonth = wonMatches.filter(x => new Date(x.date._seconds * 1000).getMonth() == today.getMonth()).length;
+      const totalPlayedMatches =  userMatches.length;
+      res.status(200).json({userMatches, pointsPerDay, totalWonMatches, totalWonMatchesThisMonth, totalPlayedMatches});
    }
-   else {
-      return res.status(400).json('Nome de usuário já existente!');
+   catch(e) {
+      console.log(e);
+      res.status(500).json(e);
    }
-}
-catch(error) {
-   console.log(error);
-   return res.status(500);
-}
 });
 
 const createUserAuthentication = async (user) => {
-await admin.auth().createUser({
-   email: user.email,
-   password: user.password,
-   displayName: user.name,
-});
+   await admin.auth().createUser({
+      email: user.email,
+      password: user.password,
+      displayName: user.name,
+   });
 }
 
 const createOfficialUser = async (user, createdUserId) => {
-delete user['password'];
-user.id = createdUserId;
-await admin.firestore().doc(`users/${createdUserId}`).create(user);
+   delete user['password'];
+   user.id = createdUserId;
+   await admin.firestore().doc(`users/${createdUserId}`).create(user);
 }
 
 const recalculatePlayersPoints = (players, winnerSide) => {
